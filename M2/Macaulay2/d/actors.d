3240243005ec72933a80bbf1af26cfcdeb3f47a8
Rickey Huang <rickeyhuang@Rickeys-MacBook-Pro.local>
2025-07-01 16:09:33 -0500

    CC division implemented

--- M2/Macaulay2/d/actors.d
+++ M2/Macaulay2/d/actors.d
@@ -509,8 +509,12 @@ export (lhs:Expr) / (rhs:Expr) : Expr := (
 	       )
      	  is y:RRcell do (					    -- # typical value: symbol /, CC, RR, CC
 	       toExpr(x.v / y.v))
+		  is y:RRicell do (  					-- # typical value: symbol /, CC, RRi, CCi
+		   toExpr(toCCi(x.v.re/y.v,x.v.im/y.v)))
      	  is y:CCcell do (					    -- # typical value: symbol /, CC, CC, CC
 	       toExpr(x.v / y.v))
+		  is y:CCicell do (					    -- # typical value: symbol /, CC, CCi, CCi
+	       toExpr(toCCi((y.v.re*x.v.re+y.v.im*x.v.im)/(y.v.re^long(2)+y.v.im^long(2)),(y.v.re*x.v.im-y.v.im*x.v.re)/(y.v.re^long(2)+y.v.im^long(2)))))
 	  is Error do rhs
 	  else binarymethod(lhs,rhs,DivideS))
      is x:RawMonomialCell do (
