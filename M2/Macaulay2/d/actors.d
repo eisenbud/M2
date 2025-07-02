79ccda80113ec7ecec412ff978fb074516a4ecc8
Rickey Huang <rickeyhuang@Rickeys-MacBook-Pro.local>
2025-07-02 11:42:11 -0500

    RRb plus RR implemented

--- M2/Macaulay2/d/actors.d
+++ M2/Macaulay2/d/actors.d
@@ -71,6 +71,9 @@ export (lhs:Expr) + (rhs:Expr) : Expr := (
 	 is y:QQcell do toExpr(
 	     RRbadd(x.v, toRRb(y.v, x.prec), x.prec),
 	     x.prec)
+	 is y:RRcell do toExpr(
+		 RRbadd(x.v, toRRb(y.v), x.prec),
+	     x.prec)
 	 is y:RRbcell do (
 	     prec := min(x.prec, y.prec);
 	     toExpr(RRbadd(x.v, y.v, prec), prec))
